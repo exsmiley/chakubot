1bd0128d9199090191350804e0f96471e06ca6e3
Zachery Miranda <zmiranda@mit.edu>
2017-04-16 13:34:16 -0400

    basic intro to chat

--- chatter.js
+++ chatter.js
@@ -10,36 +10,53 @@ class Interviewer {
 		this.client = client;
 		this.state = "beginning";
 		this.lastQuestion = -1;
-		this.client.emit('chat', "Chakubot: Hi! I'm Chakubot!");
-		this.client.emit('chat', "Chakubot: Are you ready for your interview to begin?");
+		this.sendMessage("Hi! I'm Chakubot!");
+		this.sendMessage("Are you ready for your interview to begin?");
 	}
 
-	// gets a message and dispatches a reply
+	/**
+	 * Gets a message from the client and dispatches a reply
+	 * @param message text gotten from user
+	 */
 	handleMessage(message) {
-		var that = this;
+		let that = this;
 		if(this.state === "beginning") {
 			if(message.includes("yes")) {
 				this.state = "question";
-				this.client.emit('chat', "Chakubot: Okay! Let's start the interview!");
+				this.sendMessage("Okay! Let's start the interview!");
+
+				// TODO ask a first question
 			} else {
-				this.client.emit('chat', "Chakubot: Lameeeeeeeee!");
+				this.sendMessage("Lameeeeeeeee!");
 
 				// delay 1.5 seconds before sending next message
 				setTimeout(function(){
 					if(that.state === "beginning")
-						that.client.emit('chat', "Chakubot: Are you ready for the interview yet?");
+						that.sendMessage("Now are you ready for the interview?");
 				},1500);
 			}
 		} else if(this.state == "question") {
-
+			// TODO
 		}
+	}
+
+	/**
+	 * Sends a message to the client and appends a prefix for the bot name
+	 * @param message string of text to send
+	 * @param delay number of milliseconds to delay this message
+	 */
+	sendMessage(message, delay=0) {
+		let that = this;
+		setTimeout(function(){
+			that.client.emit('chat', "Chakubot: " + message)
+		}, delay);
 	}
 }
 
-
-var chat = function(client) {
+// exported and handles the direct interactions with the socket
+const chat = function(client) {
 	console.log("a user connected")
-	var interviewer = new Interviewer(client)
+	let interviewer = new Interviewer(client)
 
 	client.on('chat', function(message) {
 		client.emit('chat', "You: " + message);
@@ -49,7 +66,12 @@ var chat = function(client) {
 			interviewer.handleMessage(message)
 		}, 500)
 		
-	})
+	});
+
+	client.on('disconnect', function(){
+		console.log('user disconnected');
+		interviewer = null;
+	});
 }
 
 
